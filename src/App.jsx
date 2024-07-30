061282b45574dd5ac17c6e9b46016a8ffccad54c
Nelzee <nelzee07@gmail.com>
2024-07-31 01:47:50 +0200

    added the user attributes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import Inputs from "./components/Inputs";
 import Display from "./components/Display";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const attributes = {
+    firstname: { value: "john", type: "text" },
+    lastname: { value: "doe", type: "text" },
+    email: { value: "john@john.com", type: "text" },
+    phone: { value: "+263772001002", type: "text" },
+    schoolName: { value: "HRE Poly", type: "text" },
+    program: { value: "IT", type: "text" },
+    companyName: { value: "InfoSec", type: "text" },
+    position: { value: "software developer", type: "text" },
+  };
+  const [name, setName] = useState("nelson");
 
   return (
     <div className="container">
-      <Inputs />
+      <Inputs attributes={attributes} setState={setName} state={name} />
       <Display />
     </div>
   );
