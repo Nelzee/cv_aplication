44e61f275325e51ec4361c5de5afa178e305fb3e
Nelzee <nelzee07@gmail.com>
2024-08-01 00:14:53 +0200

    complited the shared attributes array

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import Inputs from "./components/Inputs";
 import Display from "./components/Display";
 
 function App() {
-  const attributes = {
-    firstname: { value: "john", type: "text" },
-    lastname: { value: "doe", type: "text" },
-    email: { value: "john@john.com", type: "text" },
-    phone: { value: "+263772001002", type: "text" },
-    schoolName: { value: "HRE Poly", type: "text" },
-    program: { value: "IT", type: "text" },
-    companyName: { value: "InfoSec", type: "text" },
-    position: { value: "software developer", type: "text" },
-  };
+  const attributes = [
+    { name: "firstname", value: "john", type: "text" },
+    { name: "lastname", value: "doe", type: "text" },
+    { name: "email", value: "john@john.com", type: "text" },
+    { name: "phone", value: "+263772001002", type: "text" },
+    { name: "schoolname", value: "HRE Poly", type: "text" },
+    { name: "program", value: "IT", type: "text" },
+    { name: "companyname", value: "InfoSec", type: "text" },
+    { name: "position", value: "software developer", type: "text" },
+  ];
+
   const [name, setName] = useState("nelson");
 
   return (
     <div className="container">
       <Inputs attributes={attributes} setState={setName} state={name} />
-      <Display />
+      <Display attributes={attributes} />
     </div>
   );
 }
